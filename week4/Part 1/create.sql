b519298e979e5140575cf8065dea017b9804f6f9
Рашид Назарбеков <nazarbekovrasid@gmail.com>
2022-01-31 02:05:10 +0600

    modified

--- week4/Part 1/create.sql
+++ week4/Part 1/create.sql
@@ -3,89 +3,98 @@ CREATE DATABASE cardealership;
 USE cardealership;
 
 CREATE TABLE cars (
-  car_id int NOT NULL AUTO_INCREMENT,
-  car_brand varchar(45) NOT NULL,
-  serial_number varchar(45) NOT NULL,
-  year_of_manufacture date NOT NULL,
-  engine_capacity varchar(45) NOT NULL,
-  car_color varchar(45) NOT NULL,
-  notes varchar(45) DEFAULT NULL,
+  car_id INT UNSIGNED AUTO_INCREMENT,
+  car_brand VARCHAR(45) NOT NULL,
+  serial_number VARCHAR(45) NOT NULL,
+  year_of_manufacture DATE NOT NULL,
+  engine_capacity VARCHAR(45) NOT NULL,
+  car_color VARCHAR(45) NOT NULL,
+  price_id INT UNSIGNED NOT NULL,
+  notes VARCHAR(100) DEFAULT NULL,
   PRIMARY KEY (car_id)
   );
   
 CREATE TABLE clients (
-  clients_id int NOT NULL AUTO_INCREMENT,
-  last_name varchar(45) NOT NULL,
-  first_name varchar(45) NOT NULL,
-  phone_number varchar(45) NOT NULL,
-  notes varchar(45) DEFAULT NULL,
-  PRIMARY KEY (clients_id)
+  client_id INT UNSIGNED AUTO_INCREMENT,
+  last_name VARCHAR(45) NOT NULL,
+  first_name VARCHAR(45) NOT NULL,
+  phone_number VARCHAR(45) NOT NULL,
+  notes VARCHAR(100) DEFAULT NULL,
+  PRIMARY KEY (client_id)
 );
 
 CREATE TABLE delivery_orders (
-  delivery_order_id int NOT NULL AUTO_INCREMENT,
-  order_description varchar(45) NOT NULL,
-  placement_date date NOT NULL,
-  execution_date date NOT NULL,
+  delivery_order_id INT UNSIGNED AUTO_INCREMENT,
+  order_description VARCHAR(45) NOT NULL,
+  provider_id INT UNSIGNED NOT NULL,
+  staff_id INT UNSIGNED NOT NULL,
+  placement_date DATE NOT NULL,
+  execution_date DATE NOT NULL,
+  purchase_description_id INT UNSIGNED NOT NULL,
   PRIMARY KEY (delivery_order_id)
 );
 
 CREATE TABLE operations (
-  operation_id int NOT NULL AUTO_INCREMENT,
-  operation_date date NOT NULL,
-  operation_description varchar(45) NOT NULL,
-  factory_price double NOT NULL,
-  cost_up_to double NOT NULL,
-  total_amount double DEFAULT NULL,
+  operation_id INT UNSIGNED AUTO_INCREMENT,
+  operation_date DATE NOT NULL,
+  car_id INT UNSIGNED NOT NULL,
+  delivery_order_id INT UNSIGNED NOT NULL,
+  operation_description VARCHAR(45) NOT NULL,
+  factory_price DOUBLE NOT NULL,
+  cost_up_to DOUBLE NOT NULL,
+  total_amount DOUBLE DEFAULT NULL,
   PRIMARY KEY (operation_id)
 );
 
 CREATE TABLE orders (
-  order_id int NOT NULL AUTO_INCREMENT,
-  placement_date date NOT NULL,
-  order_number varchar(45) NOT NULL,
-  document_type varchar(45) NOT NULL,
-  document_number varchar(45) NOT NULL,
-  address varchar(45) NOT NULL,
+  order_id INT UNSIGNED AUTO_INCREMENT,
+  client_id INT UNSIGNED NOT NULL,
+  car_id INT UNSIGNED NOT NULL,
+  placement_date DATE NOT NULL,
+  order_number VARCHAR(45) NOT NULL,
+  document_type VARCHAR(45) NOT NULL,
+  document_number VARCHAR(45) NOT NULL,
+  address VARCHAR(45) NOT NULL,
+  sale_description_id INT UNSIGNED NOT NULL,
   PRIMARY KEY (order_id)
 );
 
 CREATE TABLE providers (
-  provider_id int NOT NULL AUTO_INCREMENT,
-  company_name varchar(45) NOT NULL,
-  companu_official_name varchar(45) NOT NULL,
-  position varchar(45) NOT NULL,
-  address varchar(45) NOT NULL,
-  city varchar(45) NOT NULL,
-  phone_number varchar(45) NOT NULL,
-  fax varchar(45) DEFAULT NULL,
-  payment_term varchar(45) NOT NULL,
+  provider_id INT UNSIGNED AUTO_INCREMENT,
+  company_name VARCHAR(45) NOT NULL,
+  company_official_name VARCHAR(45) NOT NULL,
+  position VARCHAR(45) NOT NULL,
+  address VARCHAR(45) NOT NULL,
+  city VARCHAR(45) NOT NULL,
+  phone_number VARCHAR(45) NOT NULL,
+  fax VARCHAR(45) DEFAULT NULL,
+  payment_term VARCHAR(45) NOT NULL,
   PRIMARY KEY (provider_id)
 );
 
 CREATE TABLE staff (
-  staff_id int NOT NULL AUTO_INCREMENT,
-  last_name varchar(45) NOT NULL,
-  first_name varchar(45) NOT NULL,
-  position varchar(45) NOT NULL,
-  phone_number varchar(45) NOT NULL,
+  staff_id INT UNSIGNED AUTO_INCREMENT,
+  last_name VARCHAR(45) NOT NULL,
+  first_name VARCHAR(45) NOT NULL,
+  position VARCHAR(45) NOT NULL,
+  phone_number VARCHAR(45) NOT NULL,
   PRIMARY KEY (staff_id)
 );
 
 CREATE TABLE prices (
-  price_id int NOT NULL AUTO_INCREMENT,
-  sale_price double NOT NULL,
+  price_id INT UNSIGNED AUTO_INCREMENT,
+  sale_price DOUBLE NOT NULL,
   PRIMARY KEY (price_id)
 );
 
 CREATE TABLE purchase_descriptions (
-  purchase_id int NOT NULL AUTO_INCREMENT,
-  description varchar(45) NOT NULL,
-  PRIMARY KEY (purchase_id)
+  purchase_description_id INT UNSIGNED AUTO_INCREMENT,
+  purchase_description VARCHAR(45) NOT NULL,
+  PRIMARY KEY (purchase_description_id)
 );
 
 CREATE TABLE sale_descriptions (
-  sale_id int NOT NULL AUTO_INCREMENT,
-  description varchar(45) NOT NULL,
-  PRIMARY KEY (sale_id)
+  sale_description_id INT UNSIGNED AUTO_INCREMENT,
+  sale_description VARCHAR(45) NOT NULL,
+  PRIMARY KEY (sale_description_id)
 );
