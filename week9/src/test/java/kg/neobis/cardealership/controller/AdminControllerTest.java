00bc40bc85541daafafb16d889153de4870306dd
Рашид Назарбеков <nazarbekovrasid@gmail.com>
2022-03-14 17:00:57 +0600

    week9

--- week9/src/test/java/kg/neobis/cardealership/controller/AdminControllerTest.java
+++ week9/src/test/java/kg/neobis/cardealership/controller/AdminControllerTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
@@ -24,5 +26,13 @@ public class AdminControllerTest {
                 .andDo(print())
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("http://localhost/login"));
+    }
+
+    @Test
+    @WithMockUser(username = "root", authorities = { "ROLE_ADMIN", "ROLE_USER" })
+    void userList2() throws Exception {
+        mockMvc.perform(get("/foradmin"))
+                .andDo(print())
+                .andExpect(authenticated());
     }
 }
